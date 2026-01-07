8ecbd4362b958521c46fb6d0bef4d3f965c5c0f9
Conner <69326416+ConConner@users.noreply.github.com>
2026-01-07 11:37:00 +0100

    feat: select multiple rooms to exclude at once in area export

--- mage/Dialogs/AreaImageExportDialog.Designer.cs
+++ mage/Dialogs/AreaImageExportDialog.Designer.cs
@@ -28,32 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button_choose = new System.Windows.Forms.Button();
             checkBox_roomRequireDoors = new System.Windows.Forms.CheckBox();
             listbox_excludedRooms = new System.Windows.Forms.ListBox();
             label_excluded = new System.Windows.Forms.Label();
             button_save = new System.Windows.Forms.Button();
-            panel1 = new mage.Controls.ExtendedPanel();
-            panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // button_choose
-            // 
-            button_choose.Location = new System.Drawing.Point(12, 12);
-            button_choose.Name = "button_choose";
-            button_choose.Size = new System.Drawing.Size(181, 21);
-            button_choose.TabIndex = 0;
-            button_choose.Text = "Choose save location";
-            button_choose.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            button_choose.UseVisualStyleBackColor = true;
-            button_choose.Click += button_choose_Click;
             // 
             // checkBox_roomRequireDoors
             // 
+            checkBox_roomRequireDoors.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             checkBox_roomRequireDoors.AutoSize = true;
             checkBox_roomRequireDoors.Checked = true;
             checkBox_roomRequireDoors.CheckState = System.Windows.Forms.CheckState.Checked;
-            checkBox_roomRequireDoors.Location = new System.Drawing.Point(0, 164);
+            checkBox_roomRequireDoors.Location = new System.Drawing.Point(12, 204);
             checkBox_roomRequireDoors.Name = "checkBox_roomRequireDoors";
             checkBox_roomRequireDoors.Size = new System.Drawing.Size(181, 19);
             checkBox_roomRequireDoors.TabIndex = 2;
@@ -62,18 +49,18 @@
             // 
             // listbox_excludedRooms
             // 
+            listbox_excludedRooms.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             listbox_excludedRooms.FormattingEnabled = true;
-            listbox_excludedRooms.ItemHeight = 15;
-            listbox_excludedRooms.Location = new System.Drawing.Point(1, 34);
+            listbox_excludedRooms.Location = new System.Drawing.Point(12, 27);
             listbox_excludedRooms.Name = "listbox_excludedRooms";
-            listbox_excludedRooms.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            listbox_excludedRooms.Size = new System.Drawing.Size(181, 124);
+            listbox_excludedRooms.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            listbox_excludedRooms.Size = new System.Drawing.Size(175, 169);
             listbox_excludedRooms.TabIndex = 3;
             // 
             // label_excluded
             // 
             label_excluded.AutoSize = true;
-            label_excluded.Location = new System.Drawing.Point(0, 16);
+            label_excluded.Location = new System.Drawing.Point(12, 9);
             label_excluded.Name = "label_excluded";
             label_excluded.Size = new System.Drawing.Size(95, 15);
             label_excluded.TabIndex = 4;
@@ -81,49 +68,36 @@
             // 
             // button_save
             // 
-            button_save.Location = new System.Drawing.Point(0, 189);
+            button_save.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            button_save.Location = new System.Drawing.Point(12, 229);
             button_save.Name = "button_save";
-            button_save.Size = new System.Drawing.Size(181, 23);
+            button_save.Size = new System.Drawing.Size(175, 23);
             button_save.TabIndex = 5;
             button_save.Text = "Save Area Image";
             button_save.UseVisualStyleBackColor = true;
             button_save.Click += button_save_Click;
             // 
-            // panel1
-            // 
-            panel1.Controls.Add(button_save);
-            panel1.Controls.Add(label_excluded);
-            panel1.Controls.Add(checkBox_roomRequireDoors);
-            panel1.Controls.Add(listbox_excludedRooms);
-            panel1.Enabled = false;
-            panel1.Location = new System.Drawing.Point(12, 39);
-            panel1.Name = "panel1";
-            panel1.Size = new System.Drawing.Size(182, 212);
-            panel1.TabIndex = 6;
-            // 
             // AreaImageExportDialog
             // 
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            ClientSize = new System.Drawing.Size(206, 263);
-            Controls.Add(panel1);
-            Controls.Add(button_choose);
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            ClientSize = new System.Drawing.Size(200, 261);
+            Controls.Add(button_save);
+            Controls.Add(checkBox_roomRequireDoors);
+            Controls.Add(label_excluded);
+            Controls.Add(listbox_excludedRooms);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "AreaImageExportDialog";
+            ShowIcon = false;
             Text = "Export Area As Image";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button_choose;
         private System.Windows.Forms.CheckBox checkBox_roomRequireDoors;
         private System.Windows.Forms.ListBox listbox_excludedRooms;
         private System.Windows.Forms.Label label_excluded;
         private System.Windows.Forms.Button button_save;
-        private System.Windows.Forms.Panel panel1;
     }
 }
